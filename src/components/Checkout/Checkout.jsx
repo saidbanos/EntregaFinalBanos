9ffee599b94614a973438e7b91e5cb9203dbb05b
Said Banos <saidbanos@gmail.com>
2023-07-19 23:04:33 -0400

    test commit

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -35,15 +35,7 @@ const Checkout = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [validated, setValidated] = useState(false);
 
-  const handleSubmit = (event) => {
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
 
-    setValidated(true);
-  };
 
   useEffect(() => {
     setIsSameEmail(emailConfirmation && email === emailConfirmation);
